8a2164131b06b356882b41439401ce0b4a08f16c
Dulshani123 <141808236+Dulshani123@users.noreply.github.com>
2023-08-14 08:28:00 +0530

    Update new_streamlit_app_2.py

--- new_streamlit_app_2.py
+++ new_streamlit_app_2.py
@@ -1,3 +1,7 @@
 import streamlit
 
 streamlit.title('My parents new Healthy Dinner')
+streamlit.header('Breakfirst Menu')
+streamlit.text('Omega 3 & Blueberry Oatmeal')
+streamlit.text('Kale, Spinach & Rocket Smothee')
+streamlit.text('Hard Bolied Free-Range Egg')
